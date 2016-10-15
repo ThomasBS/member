4b5cce8c2c8e66cfb16dff30ff87b75235ff66f5
soren <soerensrasmussen@gmail.com>
2016-10-15 21:18:16 +0200

    hmm

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+    dbhost = "localhost",
+    dbUser = "soren",
+    dbPassword = "0DD0A33184ssr",
+    dbDatabase = "member"
+}
+
+// var config = {
+//     dbhost: "localhost",
+//     dbUser: "root",
+//     dbPassword: "",
+//     dbDatabase: "rember"
+// }
+
+module.exports = config;
