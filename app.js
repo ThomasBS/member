d2b3b110e41ec35c6241fa7094817cb114df8559
ThomasBS <ts@jumdum.com>
2016-10-16 03:27:16 +0200

    Keep things DRY

--- app.js
+++ app.js
@@ -16,11 +16,8 @@ var con = mysql.createConnection({
 })
 
 con.connect(function (err) {
-    if (err) {
-      console.log('Connection error')
-    } else {
-      console.log('Database successfully connected')
-    }
+    handleError(err)
+    console.log('Database successfully connected')
 })
 
 User = require('./models/user')
@@ -31,9 +28,7 @@ Login = require('./models/login')
 /****************** User ******************/
 app.get('/users', function(req, res){
     User.getUsers(con, function(err, users){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(users)
     })
 })
@@ -41,9 +36,7 @@ app.get('/users', function(req, res){
 app.get('/users/:_id', function(req, res){
     var id = req.params._id;
     User.getUser(con, id, function(err, user){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(user)
     })
 })
@@ -51,9 +44,7 @@ app.get('/users/:_id', function(req, res){
 app.post('/users', function(req, res){
     var user = req.body;
     User.addUser(con, user, function(err, user){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(user)
     })
 })
@@ -62,18 +53,14 @@ app.put('/users/:_id', function(req, res){
     var id = req.params._id;
     var user = req.body;
     User.updateUser(con, id, user, function(err, user){
-        if (err) {
-            throw err
-        }
+        handleError(err)
     })
 })
 
 app.delete('/users/:_id', function(req, res){
     var id = req.params._id;
     User.deleteUser(con, id, function(err, user){
-        if (err) {
-            throw err
-        }
+        handleError(err)
     })
 })
 
@@ -81,9 +68,7 @@ app.delete('/users/:_id', function(req, res){
 app.get('/categories/:_userID', function(req, res){
     var id = req.params._userID;
     Category.getCategories(con, id, function(err, categories){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(categories)
     })
 })
@@ -91,9 +76,7 @@ app.get('/categories/:_userID', function(req, res){
 app.post('/categories', function(req, res){
     var category = req.body;
     Category.addCategory(con, category, function(err, category){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(category)
     })
 })
@@ -102,9 +85,7 @@ app.post('/categories', function(req, res){
 app.get('/subjects/:_categoryID', function(req, res){
     var id = req.params._categoryID;
     Subject.getSubjects(con, id, function(err, subjects){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(subjects)
     })
 })
@@ -112,9 +93,7 @@ app.get('/subjects/:_categoryID', function(req, res){
 app.post('/subjects', function(req, res){
     var subject = req.body;
     Subject.addSubject(con, subject, function(err, subject){
-        if (err) {
-            throw err
-        }
+        handleError(err)
         res.json(subject)
     })
 })
@@ -125,12 +104,19 @@ app.get('/login/:_email&:_password', function(req, res){
     var password = req.params._password
     console.log(req.params)
     Login.validate(con, email, password, function(err, login){
-        if (err) {
-          throw err
-        }
+        handleError(err)
         res.json(login)
     })
 })
 
+/**
+ * A simple function to handle the errors the app
+ * might be given.
+ */
+function handleError(err) {
+  if (err) {
+    throw err
+  }
+}
 
 app.listen(3000)
